7dd8a17e9621d9220b86f2181e39274057103259
Yand_ <anonnniymust@gmail.com>
2025-03-21 23:36:26 +0800

    Make people already login can't go into login page again

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from database import Database
 
+
 app = Flask(__name__)
 app.secret_key = "Spot"
 db = Database()
+
 
 @app.route("/")
 def home():
@@ -18,13 +20,22 @@ def login():
         try:
             db_user_data = db.call_data("Users", "password", "email", user_email)
             if user_passw == db_user_data[0]:
-                flash("Login succesfullu")
+                session.update({
+                "email": user_email,
+                "password": user_passw,
+                })
+
+                flash("Login succesfully", "info")
                 return redirect(url_for("home"))
         except:
-            flash("Couldn't find the email")
+            flash("Couldn't find the email", "info")
             return render_template("login.html")
     else:
-        return render_template("login.html")
+        if "email" in session and "password" in session:
+            flash("Welcome back", "info")
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html")
     
 
 @app.route("/register", methods=["POST", "GET"])
@@ -35,14 +46,6 @@ def register():
         user_name = request.form["name"]
         user_gender = request.form["gender"].upper()
         user_dob = request.form["dob"]
-
-        session.update({
-            "email": user_email,
-            "password": user_passw,
-            "name": user_name,
-            "gender": user_gender,
-            "dob": user_dob
-        })
 
         db.add_user(user_email, user_passw, user_name, user_gender, user_dob)
         return redirect(url_for("home"))
