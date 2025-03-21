c08a1e108527efa217dc570a13ad508d452f92ae
Yand_ <anonnniymust@gmail.com>
2025-03-21 23:22:44 +0800

    Adding mysql database and make register and login work perfectlly

---
+++ database.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="yans",
+            database="SpotSearcher"
+        )
+
+        self.mycursor = self.db.cursor()
+
+        # self.mycursor.execute("CREATE DATABASE SpotSearcher")
+        self.Q1 = """
+            CREATE TABLE Users (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                email VARCHAR(50) NOT NULL,
+                password VARCHAR(50) NOT NULL,
+                name VARCHAR(50) NOT NULL, 
+                gender ENUM('M', 'F', 'O'), 
+                dob DATE NOT NULL
+            )
+        """
+        # self.mycursor.execute(Q1)
+
+    def add_user(self, email, passw, name, gender, dob):
+        Q = "INSERT INTO Users (email, password, name, gender, dob) VALUES (%s, %s, %s, %s, %s)"
+        self.mycursor.execute(Q, (email, passw, name, gender, dob))
+        self.db.commit()
+
+    def call_data(self, table_name, wanted_entry, entry, user_entry):
+        Q = f"SELECT {wanted_entry} FROM {table_name} WHERE {entry} = %s"
+        self.mycursor.execute(Q, (user_entry,))
+        result = self.mycursor.fetchone()
+        return result
--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from database import Database
 
 app = Flask(__name__)
+app.secret_key = "Spot"
+db = Database()
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return "<p>Login in</p>"
+    if request.method == "POST":
+        user_email = request.form["email"].lower()
+        user_passw = request.form["password"]
+        try:
+            db_user_data = db.call_data("Users", "password", "email", user_email)
+            if user_passw == db_user_data[0]:
+                flash("Login succesfullu")
+                return redirect(url_for("home"))
+        except:
+            flash("Couldn't find the email")
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
+    
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        user_email = request.form["email"].lower()
+        user_passw = request.form["password"]
+        user_name = request.form["name"]
+        user_gender = request.form["gender"].upper()
+        user_dob = request.form["dob"]
+
+        session.update({
+            "email": user_email,
+            "password": user_passw,
+            "name": user_name,
+            "gender": user_gender,
+            "dob": user_dob
+        })
+
+        db.add_user(user_email, user_passw, user_name, user_gender, user_dob)
+        return redirect(url_for("home"))
+    else:
+        return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
